c85980d7512b629faf382a1425e508a8f83fa08a
MikeFreno <michaelt.freno@gmail.com>
2023-06-22 12:02:19 -0400

    remove token logging

--- src/app/ContactForm.tsx
+++ src/app/ContactForm.tsx
@@ -38,9 +38,6 @@ export default function ContactForm() {
     }
     setSendButtonLoading(false);
   };
-  useEffect(() => {
-    if (token) console.log(`hCaptcha Token: ${token}`);
-  }, [token]);
 
   return (
     <>
